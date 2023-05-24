ad997d6951024a9134ab79ef5a551da314b0f2dc
123freezebrennan <freezebrennan@gmail.com>
2023-05-24 12:08:50 -0700

    insertionSort

---
+++ dsaBasics/arrayAlgorithms/insertionSort.py
@@ -0,0 +1,37 @@
+'''
+Insertion sort
+Sorting algorithm for arrays
+
+Time Complexity: O(n * n) = O(n^2)
+Space Complexity: O(1)
+
+start with a for loop that is in range of the length of array. Set a key value 
+to the current value in index i and a value j as i - 1. Set a while loop that
+has j go backwards in the array starting from i - 1 and will stop at when
+j >= 0 and if key > arr[j], meaning that if at index j we have 5 and the key
+is 7, then it is known that the swapping values must stop as that is in a sorted
+area of the array. If the statements are false, then swap the value in index
+j + 1 with j until this break point occurs.
+
+The main for loop has n steps (length of the array) and the while loop at worst
+can do n steps (having let's say 1 being the smallest value be at the far 
+right of the array,then we must have the 1 go all the way the left side). This
+means that the time complexity is O(n * n) or O(n^2).
+'''
+
+def InsertionSort(arr):
+
+    for i in range(1, len(arr)):
+
+        key = arr[i]
+
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+
+            arr[j + 1], arr[j] = arr[j], arr[j + 1]
+
+            j -= 1
+
+    return arr
+
--- dsaBasics/arrayAlgorithms/selectionSort.py
+++ dsaBasics/arrayAlgorithms/selectionSort.py
@@ -10,9 +10,8 @@ Switch values based on the minimum value
 min_val is first set to i, an indexthen, proceeds in the for 
 loop of j which goes to i + 1 -> length of array.
 If the value in j is less than the value in the index of min_val, then the 
-min_val is set to j. 
-Finally, (not necessary but added to stop swap step,
-which can in very basic data, be another possible step) if min_val != arr[i],
+min_val is set to j.  Finally, (not necessary but added to stop swap step,
+which can be another possible unnecessary step) if min_val != arr[i],
 then swap the value in index i and index min_val.
 
 The first for loop goes through n steps, with that at worst case, 
@@ -22,16 +21,16 @@ the second for loop will go through n steps, making this process O(n^2).
 def SelectionSort(arr):
     for i in range(len(arr)):
 
-        min_val = i
+        minimum_value = i
 
         for j in range(i + 1, len(arr)):
 
-            if arr[j] < arr[min_val]:
+            if arr[j] < arr[minimum_value]:
 
-                min_val = j
+                minimum_value = j
 
-        if arr[i] != arr[min_val]:
+        if arr[i] != arr[minimum_value]:
 
-            arr[i], arr[min_val] = arr[min_val], arr[i]
+            arr[i], arr[minimum_value] = arr[minimum_value], arr[i]
 
     return arr
